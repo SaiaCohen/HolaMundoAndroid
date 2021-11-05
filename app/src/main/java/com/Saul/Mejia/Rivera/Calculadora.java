896da0ad3c3384870de1360a2a445edafe2ce9d8
SaiaCohen <mejia.rivera.saul@gmail.com>
2021-11-05 09:26:32 -0600

    Tercera Versión

---
+++ app/src/main/java/com/Saul/Mejia/Rivera/Calculadora.java
@@ -0,0 +1,29 @@
+package com.Saul.Mejia.Rivera;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Calculadora {
+
+    public void suma()
+    {
+
+    }
+    public void resta()
+    {
+
+    }
+    public void multiplicacion()
+    {
+
+    }
+    public void division()
+    {
+
+    }
+
+    }
